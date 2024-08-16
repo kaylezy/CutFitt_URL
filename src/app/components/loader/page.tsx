61e857bf034a361d76de83b13483b152a9ea57bd
Olakunle Hassan <kunlex_042003@yahoo.com>
2024-08-16 15:10:27 +0100

    updated my security keys �

---
+++ src/app/components/loader/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+// src/components/Loader.tsx
+import React from "react";
+import Image from "next/Image";
+
+const Loader: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
+      <div className="flex items-center ">
+        <div className="relative lg:flex h-full items-center justify-center ">
+          <div className="flex justify-center item-center">
+            <Image
+              className="w-50 h-50 animate-bounce"
+              src="/urlfit.png"
+              alt="icon"
+              width={150}
+              height={200}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,76 @@
+// "use client";
+
+// import React, { useState } from "react";
+// import Hero from "./components/hero/page";
+// import Feature from "./components/features/page";
+// import Footer from "./components/footer/page";
+// import Loader from "./components/loader/page";
+// import FAQ from "./pages/faq/page";
+
+// export default function Home() {
+//   const [isLoading, setIsLoading] = useState(false);
+//   return (
+//     //main section of the page
+//     <main className="overflow-hidden flex min-h-screen flex-col bg-purple-50">
+//  
+//       {/* hero component */}
+//       <Hero />
+
+//       {/* features component */}
+//       <Feature />
+
+//       {/* faq component */}
+//       <FAQ />
+
+//       {/* footer component */}
+//       <Footer />
+//     </main>
+//   );
+// }
+
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import type { AppProps } from 'next/app';
 import Hero from "./components/hero/page";
 import Feature from "./components/features/page";
 import Footer from "./components/footer/page";
-
+import Loader from "./components/loader/page";
 import FAQ from "./pages/faq/page";
 
-export default function Home() {
+const MyApp: React.FC<AppProps> = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds delay
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    //main section of the page
     <main className="overflow-hidden flex min-h-screen flex-col bg-purple-50">
-      {/* hero component */}
-      <Hero />
+      
+           {/* hero component */}
+           <Hero />
+    
+         {/* features component */}
+         <Feature />
+    
+          {/* faq component */}
+           <FAQ />
+    
+           {/* footer component */}
+           <Footer />
+         </main>
+ 
+)
+};
 
-      {/* features component */}
-      <Feature />
-
-      {/* faq component */}
-      <FAQ />
-
-      {/* footer component */}
-      <Footer />
-    </main>
-  );
-}
+export default MyApp;
