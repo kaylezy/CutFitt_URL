1e4016b14fccf0a712d56b907efe8fa8e002e761
Olakunle Hassan <kunlex_042003@yahoo.com>
2024-08-16 17:32:09 +0100

    changes made to login

--- src/app/firebase/firebase.tsx
+++ src/app/firebase/firebase.tsx
@@ -10,13 +10,20 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCSYlZLzC6hs8u6uKxDfj81Bbu2ms6jDH4",
+  authDomain: "cutfiturl-511af.firebaseapp.com",
+  projectId: "cutfiturl-511af",
+  storageBucket: "cutfiturl-511af.appspot.com",
+  messagingSenderId: "190770634669",
+  appId: "1:190770634669:web:0b90b8e672bf5cb240cb24",
+  measurementId: "G-CNDEJFJ937",
 };
 
 // Initialize Firebase
